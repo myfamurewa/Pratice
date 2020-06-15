189f43d20bb2dc6eca705e8e2e68d24a760acbb7
viscountfam <V3ctor42!>
2020-06-14 23:26:05 -0400

    tried to crate factorial function that returned a large number as a string rather than a number

--- factorial.js
+++ factorial.js
@@ -12,6 +12,21 @@ function factorial(n) {
     if(n >= 1) return n * factorial(n -1)
     else return 1;
 }
+
+//large factorials on Code Wars
+function factorial(n){
+    // Add some code
+    let value = 1
+    for(let i = 1; i < n + 1; i++){
+      value *= i
+  //     if(value === n){
+  //       return `${n}`
+  //     }
+    }
+    return value.toLocaleString('fullwide', { useGrouping: false })
+  }
+
+
 //new factorial function
 console.log("This is factorial without recursion", naiveFactorial(5))
 console.log("This is factorial with recursion", naiveFactorial(18))
