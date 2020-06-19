4b349ea32d2a10896787a9d34f6dfe14ee7564b5
viscountfam <V3ctor42!>
2020-06-19 00:10:46 -0400

    completed another easy challenge

--- EasyEdabits.js
+++ EasyEdabits.js
@@ -9,3 +9,17 @@ function firstVowel(str) {
 		return -1
 	}
 }
+
+// regex quantifier practice
+const REGEXP = /\.{3,}/g
+
+//num to arr and arr to num
+function toArray(num) {
+	let numArr = (num + "").split("")
+	let final = numArr.map(number => Number(number))
+	return final
+}
+
+function toNumber(arr) {
+	return Number(arr.join(""))
+}
