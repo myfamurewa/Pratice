ac0a6ae878c77262cb3a21e30a732b563ce01d8f
Michael Famurewa <myfamurewa@gmail.com>
2020-09-04 19:13:42 -0400

    attempting partition label

--- partitionLabels.js
+++ partitionLabels.js
@@ -1,3 +1,22 @@
 const partitionLabels = (s) => {
-    
+    if(!s){
+        return null
+    }
+    let outputarr = []
+    let last_indices = new Array(26)
+    for (let i = 0; i < s.length; i++){
+        last_indices[s.charAt(i) - 'a'] = i
+    }
+    let start = 0
+    let end = 0
+    for (let i = 0; i < s.length; i++){
+        end = Math.max(end, last_indices[s.charAt(i)-'a'])
+        if (i == end){
+            outputarr.push(end-start+1)
+        }
+    }
+    return outputarr
 }
+
+test1 = "ababcbacadefegdehijhklij"
+console.log(partitionLabels(test1))
