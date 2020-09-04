35f015e4f6c9de54f63bbcbe81e8e02731bb91ce
Michael Famurewa <myfamurewa@gmail.com>
2020-09-04 17:51:47 -0400

    passed the first test case on the repeated substring challenge on leetcode

--- repeatedSubstr.js
+++ repeatedSubstr.js
@@ -1,3 +1,13 @@
 const repeatedSubstringPattern = function(s){
-    
+    for(let i = 0; i < s.length; i++){
+        substr = s.substring(0, i)
+        if (substr === s.substring(i) || substr === s.substring(i, s.length - i)){
+            return true
+        }
+    }
+    return false
 }
+
+console.log(repeatedSubstringPattern("abab"))
+console.log(repeatedSubstringPattern("substring"))
+console.log(repeatedSubstringPattern("abcabcabcabc"))
