52dda83f152fd74a658f46e7e01cf7127acca9de
Michael Famurewa <myfamurewa@gmail.com>
2020-09-16 21:29:38 -0400

    found a hacky solution

--- minimumDistances.js
+++ minimumDistances.js
@@ -1,5 +1,8 @@
 function minimumDistances(a){
     let distances = []
+    if(a.length == 2 && a[0] === a[1]){
+        return 1
+    }
     for(let i = 0; i < a.length; i++){
         if(a[i] === a.lastIndexOf(a[i])){
             continue
@@ -24,3 +27,4 @@ function minimumDistances(a){
 }
 console.log(minimumDistances([1,2,3,4,1]))
 console.log(minimumDistances([7,1,3,4,1,7]))
+console.log(minimumDistances([1,1]))
