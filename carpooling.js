52331353f445aa38f80dd1e139ed121812a6335e
Michael Famurewa <myfamurewa@gmail.com>
2020-09-23 13:34:36 -0400

    completed the daily challenge from leet code

---
+++ carpooling.js
@@ -0,0 +1,13 @@
+var carPooling = function(trips, capacity) {
+    let currentPassengers = 0
+    let starts = []
+    let stops = []
+    let passengers = []
+    for(let i = 0; i < trips.length; i++){
+        passengers.push(trips[i][0])
+        starts.push(trips[i][1])
+        stops.push(trips[i][2])
+    }
+
+ 
+};
