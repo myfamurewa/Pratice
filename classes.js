6f39242317838e78a245be1218b0a19ec9eb45ae
Michael Famurewa <myfamurewa@gmail.com>
2020-09-29 23:38:03 -0400

    praticing with classes

--- classes.js
+++ classes.js
@@ -35,3 +35,12 @@ class Bubble {
 }
 
 setup()
+
+class Name {
+    constructor(fname, lname){
+        this.fname = `${fname[0].toUpperCase()}${fname.slice(1)}`
+        this.lname = `${lname[0]}${lname.slice(1).toLowerCase()}`
+        this.fullname = `${fname[0].toUpperCase()}${fname.slice(1)} ${lname[0]}${lname.slice(1).toLowerCase()}`
+        this.initials = `${fname[0].toUpperCase()}.${lname[0]}`
+    }
+}
