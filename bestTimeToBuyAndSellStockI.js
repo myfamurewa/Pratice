a0f59336d1d98ae41569457ffc6c3cb6359950c8
Michael Famurewa <myfamurewa@gmail.com>
2020-10-18 20:12:34 -0400

    added a slightly faster solution to best time to buy and sell stock

--- bestTimeToBuyAndSellStockI.js
+++ bestTimeToBuyAndSellStockI.js
@@ -1,16 +1,29 @@
 // first pass solution
-var maxProfit = function(prices) {
-    let bestprofit = 0;
-   for(let i = 0; i < prices.length; i++){
-        let potentialSales = prices.slice(i+1)
-        if(Math.max(...potentialSales) - prices[i] > bestprofit){
-            bestprofit = Math.max(...potentialSales) - prices[i] 
-        }
-   }
-   return bestprofit
-}
-test1 = [7,1,5,3,6,4]
-test2 = [7,6,4,3,1]
+var maxProfit = function (prices) {
+  let bestprofit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    let potentialSales = prices.slice(i + 1);
+    if (Math.max(...potentialSales) - prices[i] > bestprofit) {
+      bestprofit = Math.max(...potentialSales) - prices[i];
+    }
+  }
+  return bestprofit;
+};
+test1 = [7, 1, 5, 3, 6, 4];
+test2 = [7, 6, 4, 3, 1];
 
-console.log(maxProfit(test1))
-console.log(maxProfit(test2))
+console.log(maxProfit(test1));
+console.log(maxProfit(test2));
+
+var maxProfitV2 = function (prices) {
+  let max = 0
+  let min = Infinity
+  for(let i = 0; i < prices.length; i++){
+      if(prices[i] < min) {
+          min = prices[i]
+      } else {
+          max = Math.max(max, prices[i] - min)
+      }
+  }
+  return max
+};
