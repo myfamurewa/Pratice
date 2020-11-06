5b5258ee96ea99e2438f0434c7556becaf264191
Michael Famurewa <myfamurewa@gmail.com>
2020-11-06 11:53:34 -0500

    a binary search is the best way to solve this problem in an efficient manner

--- SmallestDivisor/solution.js
+++ SmallestDivisor/solution.js
@@ -22,3 +22,50 @@ test1 = smallestDivisor([1,2,5,9], 6)
 test2 = smallestDivisor([2,3,5,7,11], 11)
 console.log(test1)
 console.log(test2)
+
+function computeSum(i) {
+    let sum = 0;
+    for (const num of nums) {
+        sum+= Math.ceil(num / i);
+    }
+    return sum
+}
+let lo = 1
+let hi = 2
+
+while (computeSum(hi) > threshold) {
+    lo = hi;
+    hi = hi * 2;
+}
+// now the lo and high are range of the answer
+while (lo <= hi) {
+    const mid = Math.floor((lo + hi) / 2)
+    const sum = computeSum(mid)
+    
+    if (sum > threshold) {
+        lo = mid + 1
+    } else {
+        hi = mid -1
+    }
+}
+
+var smallestDivisor = function (nums, threshold) {
+    let l = 1, r = Math.max(...nums);
+
+    const passed = (d) => {
+        const sum = nums
+            .map(n => Math.ceil(n / m))
+            .reduce((p, v) => p += v, 0);
+        return sum <= threshold;
+    }
+
+    while (l < r) {
+        m = l + Math.floor((r - l) / 2);
+        if (passed(m)) {
+            r = m;
+        } else {
+            l = m + 1;
+        }
+    }
+    return l;
+};
