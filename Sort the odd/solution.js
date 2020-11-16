3365f6b1e29988eb29fd0e7683a237e0eabaae08
Michael Famurewa <myfamurewa@gmail.com>
2020-11-16 11:44:20 -0500

    added a easier to read version

--- Sort the odd/solution.js
+++ Sort the odd/solution.js
@@ -2,3 +2,17 @@ function sortArray(array) {
     const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
     return array.map((x) => x % 2 ? odd.shift() : x);
   }
+
+  function sortArray(array) {
+    // Return a sorted array.
+    length = array.length
+    if(length <= 1){
+      return array
+    } else {
+      let indices = []
+      array.filter((value, index) => value % 2 && indices.push(index))
+      .sort((a, b) => a - b)
+      .forEach((value, index) => array[indices[index]] = value)
+      return array
+    }
+  }
