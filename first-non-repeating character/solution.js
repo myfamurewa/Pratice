00b925abd03028e487fbd7029eb26176ec09f38f
Michael Famurewa <myfamurewa@gmail.com>
2020-11-17 12:41:07 -0500

    making improvements

--- first-non-repeating character/solution.js
+++ first-non-repeating character/solution.js
@@ -5,13 +5,14 @@ function firstNonRepeatingLetter(s) {
     let count = {}
     let id = 0
     for(let i = 0; i < s.length; i++){
-      if(count[s[i]]){
+      if(count[s[i].toLowerCase()]){
         count[s[i]].value += 1
       } else {
-        count[s[i]] = {id: id, value: 1}
+        s[i].toLowerCase() === s[i] ? count[s[i]] = {id: id, value: 1, uppercase: false} : count[s[i].toLowerCase()] = {id: id, value: 1, uppercase: true}
         id++
       }
     }
-    result = Object.entries(count).filter(count => count[1].value === 1)
-    return result[0][0]
+    let result = Object.entries(count).filter(count => count[1].value === 1).sort((a, b) => a - b)
+    console.log("s", s, "result", result)
+    return result.length? result[0][0].uppercase === true ? result[0][0].toUpperCase(): result[0][0] : ""
   }
