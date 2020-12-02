f12734304ef503e1df3457ef8b32b4568c3f9f91
Michael Famurewa <myfamurewa@gmail.com>
2020-12-01 22:05:22 -0500

    completed with an alt solution

--- First and Last Position in a Sorted Array/solution.ts
+++ First and Last Position in a Sorted Array/solution.ts
@@ -9,3 +9,16 @@ function searchRange(nums: number[], target: number): number[] {
     }
     return [firstIndex, lastIndex]
 };
+
+function searchRangeAlt(nums: number[], target: number): number[] {
+    let ret = [-1, -1]
+    for (let i = 0; i < nums.length; i ++){
+        if(nums[i] === target){
+            ret[1] = i
+            if(ret[0] === -1){
+                ret[0] = i
+            }
+        }
+    }
+    return ret
+}
