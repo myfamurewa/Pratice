f3c1d5457c1db9e1838f44d1b491b2fad51ce0da
Michael Famurewa <myfamurewa@gmail.com>
2020-12-09 11:10:53 -0500

    added Treenode definition

--- Flatten binary tree to linked list/solution.ts
+++ Flatten binary tree to linked list/solution.ts
@@ -1,16 +1,27 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
 function flatten(root: TreeNode | null): void {
-    if(root === null){
-        return
+  if (root === null) {
+    return;
+  }
+  if (root.left) {
+    let last = root.left;
+    while (last.right !== null) {
+      last = last.right;
     }
-    if (root.left){
-        let last = root.left;
-        while (last.right !== null){
-            last = last.right
-        }
-        let tmp = root.right;
-        root.right = root.left;
-        last.right = tmp
-        root.left = null
-    }
-    flatten(root.right)
-};
+    let tmp = root.right;
+    root.right = root.left;
+    last.right = tmp;
+    root.left = null;
+  }
+  flatten(root.right);
+}
