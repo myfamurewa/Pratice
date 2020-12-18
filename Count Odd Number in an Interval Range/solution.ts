fc4feeb62d635384278d2c32000436f0bccae49b
Michael Famurewa <myfamurewa@gmail.com>
2020-12-18 18:03:48 -0500

    a marginal improvement on count odds

--- Count Odd Number in an Interval Range/solution.ts
+++ Count Odd Number in an Interval Range/solution.ts
@@ -7,3 +7,19 @@ function countOdds(low: number, high: number): number {
     }
     return count
 };
+
+function countOddsV2(low: number, high: number): number {
+    let count = 0;
+    let start: number;
+    if(low%2 === 1){
+        start = low
+    } else {
+        start = low + 1
+    }
+    for(let i = start; i <= high; i+= 2){
+        if(i % 2 === 1){
+            count++
+        }
+    }
+    return count
+}
