0cb469a745fd98f149b4fedb1e22f005115578a3
viscountfam <V3ctor42!>
2020-12-17 21:11:42 -0500

    I need to make the solution faster the current on times out

---
+++ Strongest Even Number/solution.ts
@@ -0,0 +1,30 @@
+function strongestEven(n: number, m: number): number {
+    let strongest = 0;
+    let num: number;
+    for(let i = n; i <= m; i++){
+        if(i % 2 === 0){
+            let currentStrength = findStrength(i)
+         if(currentStrength > strongest){
+            strongest = currentStrength
+            num = i
+         }
+        } else {
+            continue
+        }
+    }
+    return num
+}
+
+function findStrength(n: number): number {
+    let strength = 0
+    while(n % 2 === 0){
+        n /= 2
+        strength++
+    }
+    return strength
+}
+
+console.log(findStrength(16))
+console.log(strongestEven(1,2))
+console.log(strongestEven(5,10))
+console.log(strongestEven(48, 56))
