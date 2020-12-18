45e6b417e65606b1eca5a68ca95d05aa36f81828
viscountfam <V3ctor42!>
2020-12-17 21:55:38 -0500

    attempted an alternate solution?

--- Strongest Even Number/solution.ts
+++ Strongest Even Number/solution.ts
@@ -14,7 +14,20 @@ function strongestEven(n: number, m: number): number {
     }
     return num
 }
+function strongestEvenV2(n: number, m: number): number {
+    if(n < m) return -1
+    let best = 1;
+    while(true){
+        let ceiling = n / best + (n % best === 0? 0 : 1);
+        let floor = m / best;
+        if (ceiling > floor) {
+            best = best / 2;
 
+            return best * (n/ best)
+        }
+        best *= 2;
+    }
+}
 function findStrength(n: number): number {
     let strength = 0
     while(n % 2 === 0){
