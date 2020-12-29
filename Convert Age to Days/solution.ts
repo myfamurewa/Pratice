17fb92d863749d4d7eb4e27a616755ca1ea41aaa
Michael Famurewa <myfamurewa@gmail.com>
2020-12-28 21:08:23 -0500

    some more useful functions

--- Convert Age to Days/solution.ts
+++ Convert Age to Days/solution.ts
@@ -4,10 +4,33 @@ function calcAge(age: number): number {
 
 console.log(calcAge(54))
 
-function calcAgeAdvanced(birthdate: string | number): number {
+function calcAgeAdvanced(birthdate: string | number): string {
     if(typeof birthdate === "string"){
         birthdate = Date.parse(birthdate)
     }
     let now = Date.now()
-    return 0
+    let diffTime = Math.abs(now - birthdate)
+    let diffYears = Math.floor(diffTime/ (1000 * 60 * 60 * 24 * 365))
+    let diffDays = diffYears > 1? Math.floor((diffTime - diffYears * 1000 * 60 * 60 * 24 * 365) / (1000 * 60 * 60 * 24)) : Math.floor(diffTime / (1000 * 60 * 60 * 24))
+    return diffDays > 0? `${diffYears} years and ${diffDays} days`: "it's the same day"
 }
+
+function calcAgeAtSpecificDate(birthdate: string | number, specificDate: string | number): string {
+    if(typeof birthdate === "string"){
+        birthdate = Date.parse(birthdate)
+    }
+    if(typeof specificDate === "string"){
+        specificDate = Date.parse(specificDate)
+    }
+    let diffTime = Math.abs(specificDate - birthdate)
+    let diffYears = Math.floor(diffTime/ (1000 * 60 * 60 * 24 * 365))
+    let diffDays = diffYears > 1? Math.floor((diffTime - diffYears * 1000 * 60 * 60 * 24 * 365) / (1000 * 60 * 60 * 24)) : Math.floor(diffTime / (1000 * 60 * 60 * 24))
+    return diffDays > 0? `${diffYears} years and ${diffDays} days`: "it's the same day"
+
+}
+
+console.log(calcAgeAdvanced("7/12/2020"))
+console.log(calcAgeAdvanced("12/28/2020"))
+console.log(calcAgeAdvanced("12/25/2020"))
+console.log(calcAgeAdvanced("05/08/1996"))
+console.log(calcAgeAdvanced(0))
