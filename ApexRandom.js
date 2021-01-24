fc8a0bb577cfcd350a970580f5bf2d7435b9617a
Michael Famurewa <myfamurewa@gmail.com>
2021-01-23 22:35:45 -0500

    added a new legend to ApexRandom

--- ApexRandom.js
+++ ApexRandom.js
@@ -1,4 +1,4 @@
-let legends = ["Bloodhound", "Gibraltar", "Lifeline", "Wraith", "Caustic", "Mirage", "Octane", "Wattson", "Revenant", "Loba", "Rampart", "Horizon"]
+let legends = ["Bloodhound", "Gibraltar", "Lifeline", "Wraith", "Caustic", "Mirage", "Octane", "Wattson", "Revenant", "Loba", "Rampart", "Horizon", "Fuze"]
 
 function randomLegend(){
     let rand = Math.floor(Math.random() * legends.length)
