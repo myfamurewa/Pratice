b3669b453d243493998b3416eaa48b036396d803
Michael Famurewa <myfamurewa@gmail.com>
2021-01-28 15:03:16 -0500

    get smallest string from leetcode

---
+++ Smallest String With A Given Numeric Value/solution.ts
@@ -0,0 +1,15 @@
+function getSmallestString(n: number, k: number): string {
+    const alphabet = ['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    let sum = 0
+    let s = ''
+    
+    for (let i = n - 1; i >= 0; i--) {
+        let target = (k - i) - sum
+        if (target > 26) target = 26
+        s = alphabet[target] + s
+        sum += target
+    }
+
+    return s
+};
+
