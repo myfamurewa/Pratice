383b0ed655f0c9bc05855510f25d360054f4daed
Michael Famurewa <myfamurewa@gmail.com>
2021-02-16 19:49:06 -0500

    added a constant memory solutions fro first missing positive

--- First Missing Positive/solution.ts
+++ First Missing Positive/solution.ts
@@ -21,6 +21,22 @@ function firstMissingPositiveAlt(nums: number[]): number {
     }
     return lowest
 }
+
+function firstMissingPositiveConstmem(nums: number[]): number {
+    for(let i = 0; i < nums.length; i++){
+        while(nums[i]>0 && nums[i] < nums.length && nums[i] !== i + 1 && nums[i] !== nums[nums[i] - 1]){
+            let temp = nums[i]
+            nums[i] = nums[temp - 1]
+            nums[temp - 1] = temp
+        }
+    }
+    for(let j = 0; j < nums.length; j++){
+        if(nums[j] !== j + 1){
+            return j + 1
+        }
+    }
+    return nums.length + 1
+}
 let test12 = [1,2,0]
 let test13 = [3,4,-1,1]
 let test14 = [7,8,9,11,12]
