62e51afa54814d4dff5331c3f9979705bc7dba23
Michael Famurewa <myfamurewa@gmail.com>
2021-02-16 19:32:19 -0500

    a slightly different solution that deals with duplicates without a set

--- First Missing Positive/solution.ts
+++ First Missing Positive/solution.ts
@@ -11,9 +11,18 @@ function firstMissingPositive(nums: number[]): number {
     return correctNum 
 };
 
+function firstMissingPositiveAlt(nums: number[]): number {
+    let newNums = nums.sort((a, b) => a - b).filter(num => num > 0)
+    let lowest = 1
+    for(let num of newNums){
+        if(num === lowest){
+            lowest++
+        }
+    }
+    return lowest
+}
 let test12 = [1,2,0]
 let test13 = [3,4,-1,1]
 let test14 = [7,8,9,11,12]
 let test15 = [1,2,3,4,5,6,7,8,9,20]
 
-console.log(firstMissingPositive(test15))
