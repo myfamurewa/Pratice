ac0d5525027fed6c1b58dc484cb2bc1d14c5be74
Michael Famurewa <myfamurewa@gmail.com>
2021-02-20 21:25:25 -0500

    removed the parse int from the function and replaced it with an object for the digits

--- Str to Num/solution.ts
+++ Str to Num/solution.ts
@@ -6,13 +6,24 @@ function strToNum(str: string): number {
     if(!isNumeric(str)){
         return -1
     }
+    let digits = {
+        "1": 1,
+        "2": 2,
+        "3": 3, 
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+    }
     let value = 0
     let tensPlace = 0
     for(let i = str.length - 1; i >= 0; i--){
         if(!isNumeric(str[i])){
             continue
         }
-        value += parseInt(str[i]) * Math.pow(10, tensPlace)
+        value += digits[str[i]] * Math.pow(10, tensPlace)
         tensPlace++
     }
     return str[0] === "-"? value * -1 : value;
@@ -21,3 +32,7 @@ function strToNum(str: string): number {
 console.log(strToNum("17"))
 console.log(strToNum("-56"))
 console.log(strToNum("4321"))
+console.log(strToNum("Apple"))
+console.log(strToNum("R301"))
+console.log(strToNum("P-2020"))
+console.log(strToNum("12.2"))
