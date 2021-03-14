87aca9a27b2056b6c7084f7ed79583d08249bc00
Michael Famurewa <myfamurewa@gmail.com>
2021-03-13 23:52:05 -0500

    a new legend will arrive soon (hopefully)

--- ApexRandom.js
+++ ApexRandom.js
@@ -1,8 +1,8 @@
-let legends = ["Bloodhound", "Gibraltar", "Lifeline", "Wraith", "Caustic", "Mirage", "Octane", "Wattson", "Revenant", "Loba", "Rampart", "Horizon", "Fuze"]
+let legends = ["Bloodhound", "Gibraltar", "Lifeline", "Wraith", "Caustic", "Mirage", "Octane", "Wattson", "Revenant", "Loba", "Rampart", "Horizon", "Fuze", "Ashe"]
 
 function randomLegend(){
     let rand = Math.floor(Math.random() * legends.length)
     return legends[rand]
 }
 
-console.log(randomLegend())
+console.log(randomLegend(), randomLegend(), randomLegend())
---
+++ Basic Calculator II/solution.ts
@@ -0,0 +1,4 @@
+function calculate(s: string): number {
+let stack = []
+
+};
